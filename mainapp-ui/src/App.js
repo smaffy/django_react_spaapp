412b4cebf117074afecc7a501ca75942530cb1f1
smaffy <jekrudcz@gmail.com>
2020-11-28 17:56:02 +0200

    axios and test api

--- mainapp-ui/src/App.js
+++ mainapp-ui/src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+    const [people, setPeople] = useState([])
+
+    useEffect(() => {
+        axios({
+            method: "GET",
+            url: "http://127.0.0.1:8000/api/test-api/"
+        }).then(response => {
+            setPeople(response.data)
+        })
+    }, [])
+
   return (
     <div className="App">
       <h1> Hello World! React + Django </h1>
+        <ul>
+            {people.map(p => (
+                <li key={p.id}>{p.name}</li>
+            ))}
+        </ul>
 
     </div>
   );
---
+++ mainapp/api/views.py
@@ -0,0 +1,14 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+
+class TestAPIView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        data = [{"id": 1, "name": "Jeremy"}, {"id": 2, "name": "Valery"}]
+        return Response(data)
+
+
+
+
+
